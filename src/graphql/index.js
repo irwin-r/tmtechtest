e98e2fcdcea10f529b6d883be7fea26d7bbc2c1e
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2019-12-23 23:59:33 +1100

    Created index file.

---
+++ src/graphql/index.js
@@ -0,0 +1,4 @@
+export { default as createContext } from './createContext';
+export { default as dataSources } from './dataSources';
+export { default as resolvers } from './resolvers';
+export { default as typeDefs } from './typeDefs';
