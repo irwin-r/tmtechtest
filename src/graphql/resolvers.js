c828c12ae2761998210d4735823253b55c8a7b10
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2019-12-24 00:31:05 +1100

    Updated to leverage apollo-resolvers' combineResolvers method.

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,39 +1,30 @@
+import { combineResolvers } from 'apollo-resolvers';
 import { DateTimeResolver, EmailAddressResolver } from 'graphql-scalars';
 
-import {
-	login,
-	logout,
-	removeFavorite,
-	saveFavorite,
-} from './resolvers/mutations';
+import * as mutations from './resolvers/mutations';
+import * as queries from './resolvers/queries';
 
-import { job, jobs, user } from './resolvers/queries';
-
-export default {
-	DateTime: DateTimeResolver,
-
-	EmailAddress: EmailAddressResolver,
-
-	Query: {
-		job,
-		jobs,
-		user,
+export default combineResolvers([
+	{
+		DateTime: DateTimeResolver,
+		EmailAddress: EmailAddressResolver,
 	},
+	mutations.login,
+	mutations.logout,
+	mutations.removeFavorite,
+	mutations.saveFavorite,
+	queries.job,
+	queries.jobs,
+	queries.user,
+	{
+		User: {
+			favorites(parent, _, { dataSources: { jobService, userService } }) {
+				const favoritesIds = Array.from(
+					userService.findById(parent.id).favorites
+				);
 
-	Mutation: {
-		login,
-		logout,
-		removeFavorite,
-		saveFavorite,
-	},
-
-	User: {
-		favorites(parent, _, { dataSources: { jobService, userService } }) {
-			const favoritesIds = Array.from(
-				userService.findById(parent.id).favorites
-			);
-
-			return favoritesIds.map(jobId => jobService.findById(jobId));
+				return favoritesIds.map(jobId => jobService.findById(jobId));
+			},
 		},
 	},
-};
+]);
