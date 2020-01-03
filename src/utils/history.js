14078f8440e42f9cf87543e6cdd35f623ff72041
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2020-01-03 14:28:08 +1100

    Removed unneeded utility methods.

--- src/utils/history.js
+++
@@ -1,8 +0,0 @@
-// Browser History API helpers
-
-export const replace = (path, title = null) =>
-	window.history.replaceState({}, title, path);
-export const push = (path, title = null) =>
-	window.history.pushState({}, title, path);
-
-export const history = { push, replace };
