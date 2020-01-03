9120230155c10e13138657ff0f9ce47521e894db
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2020-01-04 01:04:06 +1100

    Updated App to inject Apollo & User context.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,41 +1,34 @@
-import App, { Container } from 'next/app';
-import fetch from 'isomorphic-unfetch';
-import getConfig from 'next/config';
+import { ApolloProvider } from '@apollo/react-hooks';
+import NextApp from 'next/app';
 import React from 'react';
 
-import { UserProvider } from '../components/UserContext';
-import Navbar from '../components/Navbar';
+import { NavBar, UserProvider } from '../components';
+import { withApollo } from '../hoc';
 
-const {
-	publicRuntimeConfig: { apiEndpoint },
-} = getConfig();
+class App extends NextApp {
+	static async getInitialProps({ Component, ctx }) {
+		const pageProps = Component.getInitialProps
+			? await Component.getInitialProps(ctx)
+			: {};
 
-export default class MyApp extends React.Component {
-	static async getInitialProps({ Component, router, ctx }) {
-		let pageProps = {};
-
-		// We need to forward the request headers on the server.
-		const url = `${apiEndpoint}/session`;
-		const headers = ctx.req ? { cookie: ctx.req.headers.cookie } : undefined;
-
-		const { user } = await fetch(url, { headers }).then(res => res.json());
-
-		if (Component.getInitialProps) {
-			pageProps = await Component.getInitialProps({ user, ...ctx });
-		}
-
-		return { pageProps, user };
+		return { pageProps };
 	}
 
-	render() {
-		const { Component, pageProps, user } = this.props;
+	render = () => {
+		const { apollo, Component, pageProps, user } = this.props;
+
 		return (
-			<UserProvider initialUserValue={user}>
-				<div className="App">
-					<Navbar />
-					<Component {...pageProps} />
-				</div>
-			</UserProvider>
+			<ApolloProvider client={apollo}>
+				<UserProvider value={user}>
+					<div className="App">
+						<NavBar />
+						{/* eslint-disable-next-line react/jsx-props-no-spreading */}
+						<Component {...pageProps} />
+					</div>
+				</UserProvider>
+			</ApolloProvider>
 		);
-	}
+	};
 }
+
+export default withApollo(App);
