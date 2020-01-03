f2b06f866beead786bf5ae8c710620291c2d57f5
Irwin Razaghi <irwin.razaghi@oohmedia.com.au>
2020-01-04 00:44:49 +1100

    Removed superfluous legacy dynamic route handler from server.

--- src/server.js
+++ src/server.js
@@ -40,15 +40,9 @@ import { getJwtSecret, getListenPort, isDevelopmentEnvironment } from './utils';
 		})
 	);
 
-	apollo.applyMiddleware({ app: express, bodyParserConfig: true });
+	apollo.applyMiddleware({ app: express, bodyParserConfig: true, cors: true });
 
 	express.use(Express.static('../public'));
-
-	express.get('/job/:jobId', (req, res) => {
-		const { jobId } = req.params;
-
-		next.render(req, res, '/job', { jobId, ...req.query });
-	});
 
 	express.get('*', next.getRequestHandler());
 
